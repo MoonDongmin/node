8580fd59ce787f53cb121363d7cc07b323656eb1
MoonDongMin <20191836@o365.hanbat.ac.kr>
2023-01-14 18:48:37 +0900

    study Chap04

--- Chapter04/4.1/server1.js
+++ Chapter04/4.1/server1.js
@@ -1,10 +1,11 @@
-const http = require('http');
+//const http = require('http');
+import http from 'http';
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
   res.write('<h1>Hello Node!</h1>');
   res.end('<p>Hello Server!</p>');
 })
-  .listen(808, () => { // 서버 연결
+  .listen(18080, () => { // 서버 연결
     console.log('808번 포트에서 서버 대기 중입니다!');
   });
