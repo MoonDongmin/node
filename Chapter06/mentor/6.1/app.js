87d0125d8f0cfea8ba582f1de4981fe22c7f18d9
MoonDongmin <20191836@o365.hanbat.ac.kr>
2024-04-18 10:55:41 +0900

    node 세미나-맨토

--- Chapter06/mentor/6.1/app.js
+++ Chapter06/mentor/6.1/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
+import "dotenv/config";
 
 import * as path from "path";
 
 const __dirname = path.resolve();
-
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -13,17 +13,18 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get('/', (req, res, next) => {
-    console.log('GET / 요청에서만 실행됨');
-    next();
-}, (req, res) => {
-    throw new Error('에러는 에러 처리 미들웨어로 감');
-});
-
-// app.get('/', (req, res) => {
-//     // res.send('Hello, Express');
-//     res.sendFile(path.join(__dirname, '/index.html'));
+// app.get('/', (req, res, next) => {
+//     console.log('GET / 요청에서만 실행됨');
+//     next();
+// }, (req, res) => {
+//     throw new Error('에러는 에러 처리 미들웨어로 감');
 // });
+
+app.get('/', (req, res) => {
+    // res.send('Hello, Express');
+    // res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(process.cwd(), '/index.html'));
+});
 
 app.use((err, req, res, next) => {
     console.error(err)
@@ -34,3 +35,5 @@ app.use((err, req, res, next) => {
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기중');
 })
+
+// app.listen(app.get('port'));
--- Chapter06/mentor/6.2/app.js
+++ Chapter06/mentor/6.2/app.js
@@ -8,11 +8,12 @@ import path from "path";
 const __dirname = path.resolve();
 
 dotenv.config();
+
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(process.cwd(), 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
