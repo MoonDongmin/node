4e7075a8a3b01327f70f49cad0803ec39392e9e9
MoonDongmin <20191836@o365.hanbat.ac.kr>
2024-05-23 16:15:02 +0900

    HTTPS 설정

--- MentorProject/src/main/app.js
+++ MentorProject/src/main/app.js
@@ -1,13 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import * as https from "node:https";
 
 dotenv.config();
 
 const app = new express();
+const options = {
+  key: fs.readFileSync(path.join(process.cwd()+'/resource/cert/privkey.pem')),
+  cert:fs.readFileSync(path.join(process.cwd()+'/resource/cert/fullchain.pem')),
+};
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Hello Node.js"));
 
-app.listen(port, () => {
+https
+    .createServer(options,app)
+    .listen(port, () => {
   console.log(`${port}번에서 대기중`);
 });
+
+
+
